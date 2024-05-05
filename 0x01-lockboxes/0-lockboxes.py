b5c33116f7b3e887376bb52a462791a0368b4c59
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2024-05-05 03:21:47 +0300

    fixed error output

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,13 +2,20 @@
 """
 Solution to lockboxes problem
 """
+
+
 def canUnlockAll(boxes):
     """
     Determines whether a series of locked boxes can be opened
     """
     n = len(boxes)
+    if n == 0:
+        return False
+
     visited = set()
     keys = set(boxes[0])
+
+    visited.add(0)  # The first box is always unlocked
 
     while keys:
         box_key = keys.pop()
