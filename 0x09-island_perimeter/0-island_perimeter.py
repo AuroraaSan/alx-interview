77b2f9591cbc47366acc050df4ee2ab9b6466274
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2024-08-28 15:09:15 +0300

    addition problem solved

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
-
-""" Contains makeChange function"""
-
+"""
+Contains island_perimeter function
+"""
 
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid
     Args:
-        grid: 2d list of integers containing 0(water) or 1(land)
+        grid: 2D list of integers containing 0 (water) or 1 (land)
     Return:
         the perimeter of the island
     """
     perimeter = 0
     for row in range(len(grid)):
-      for col in range(len(grid[0])):
-        if grid[row][col] == 1:
-          perimeter +=4
-          if (row-1 >= 0 and grid[row-1][col]== 1 ):
-            perimeter -= 1
-          if (col -1 >= 0 and grid[row][col - 1] == 1 ):
-            perimeter -= 1
-          if (row + 1 >= 0 and grid[row + 1][col] == 1):
-            perimeter -= 1
-          if (col + 1 >= 0 and grid[row][col + 1] == 1):
-            perimeter -= 1
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                # Check the top
+                if row - 1 >= 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+
+                # Check the left
+                if col - 1 >= 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+
+                # Check the bottom
+                if row + 1 < len(grid) and grid[row + 1][col] == 1:
+                    perimeter -= 1
+
+                # Check the right
+                if col + 1 < len(grid[0]) and grid[row][col + 1] == 1:
+                    perimeter -= 1
+
     return perimeter
