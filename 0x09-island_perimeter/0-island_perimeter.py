bf76996f74c006131429665823161dc5cb119286
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2024-08-28 14:18:00 +0300

    task done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
+
+""" Contains makeChange function"""
+
+
 def island_perimeter(grid):
-  perimeter = 0
-  for row in range(len(grid)):
-    for col in range(len(grid[0])):
-      if grid[row][col] == 1:
-        perimeter +=4
-        if (row-1 >= 0 and grid[row-1][col]== 1 ):
-          perimeter -= 1
-        if (col -1 >= 0 and grid[row][col - 1] == 1 ):
-          perimeter -= 1
-        if (row + 1 >= 0 and grid[row + 1][col] == 1):
-          perimeter -= 1
-        if (col + 1 >= 0 and grid[row][col + 1] == 1):
-          perimeter -= 1
-  return perimeter
+    """
+    Returns: perimeter of island
+    grid:
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+      for col in range(len(grid[0])):
+        if grid[row][col] == 1:
+          perimeter +=4
+          if (row-1 >= 0 and grid[row-1][col]== 1 ):
+            perimeter -= 1
+          if (col -1 >= 0 and grid[row][col - 1] == 1 ):
+            perimeter -= 1
+          if (row + 1 >= 0 and grid[row + 1][col] == 1):
+            perimeter -= 1
+          if (col + 1 >= 0 and grid[row][col + 1] == 1):
+            perimeter -= 1
+    return perimeter
