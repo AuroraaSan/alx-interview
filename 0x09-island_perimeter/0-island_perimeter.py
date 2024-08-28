c7715f9dfc99b1ef7d52245ecc85915b00135a0b
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2024-08-28 15:09:58 +0300

    addition problem solved

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,22 +15,21 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
-                perimeter += 4
 
                 # Check the top
                 if row - 1 >= 0 and grid[row - 1][col] == 1:
-                    perimeter -= 1
+                    perimeter += 1
 
                 # Check the left
                 if col - 1 >= 0 and grid[row][col - 1] == 1:
-                    perimeter -= 1
+                    perimeter += 1
 
                 # Check the bottom
                 if row + 1 < len(grid) and grid[row + 1][col] == 1:
-                    perimeter -= 1
+                    perimeter += 1
 
                 # Check the right
                 if col + 1 < len(grid[0]) and grid[row][col + 1] == 1:
-                    perimeter -= 1
+                    perimeter += 1
 
     return perimeter
